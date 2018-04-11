c42158d412ccf10bd90749243806243914f02c70
Leticia Garcia <leti.garcia.martin@gmail.com>
2018-04-11 21:33:24 +0200

    Preprocessor for adding name field

--- jupyxplorer/main.py
+++ jupyxplorer/main.py
@@ -8,25 +8,21 @@ import nbformat
 class FillName(Preprocessor):
     """A preprocessor to fill the name of a field on some of the cells of a notebook"""
 
-    field   = Unicode("field", help="name of the field to explore")
-    field.tag(config='True')
+    field   = Unicode(u'field_name', help='name of the field to explore').tag(config=True)
 
     def preprocess(self, nb, resources):
-        self.log.info("I'll keep only cells from %d", self.field)
+        self.log.info("I'll add %d as name of the field to explore", self.field)
         for cell in nb.cells:
-            print(cell.metadata)
-            # if cell.metadata.name == "lae":
-            #     print("LAENK")
+            cell.source = cell.source.format(self.field)
         return nb, resources
 
 
 c = Config()
-c.FillName.field = "acquirer"
+c.FillName.field = 'acquirer'
 c.NotebookExporter.preprocessors = [FillName]
+c.FillName.enabled = True
 
 exporter = NotebookExporter(config=c)
-
 notebook = nbformat.read('../notebooks/index.ipynb', as_version=4)
 
-# Process the notebook
 print(exporter.from_notebook_node(notebook)[0])
