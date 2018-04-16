62497799e9f60294e661674f7f8276a876576c4e
laetitiae <leti.garcia.martin@gmail.com>
2018-04-16 16:41:49 +0200

    Fix path and sort imports

--- jupyxplorer/main.py
+++ jupyxplorer/main.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-import logging
-import sys
 import argparse
+import logging
+import os
+import sys
+
 import nbformat
 
-from traitlets import Unicode
-from traitlets.config import Config
-from nbconvert.preprocessors import Preprocessor
 from nbconvert.exporters import NotebookExporter
+from traitlets.config import Config
 
-from jupyxplorer.parser import load_yml, validate_data, parser_errors
+from .parser import load_yml, validate_data, parser_errors
 from .processors import FillName
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-logger = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 def main(argv=sys.argv[1:]):
@@ -33,14 +35,14 @@ def main(argv=sys.argv[1:]):
             c.FillName.enabled = True
 
             exporter = NotebookExporter(config=c)
-            notebook = nbformat.read("../notebooks/{}.ipynb".format(field["type"]), as_version=4)
+            notebook = nbformat.read(os.path.join(BASE_DIR, '../notebooks/{}.ipynb'.format(field["type"])), as_version=4)
 
             print(exporter.from_notebook_node(notebook)[0])
     elif type(data) is str:
-        logger.error("YAMLError: {}".format(data))
+        LOG.error("YAMLError: {}".format(data))
     else:
-        logger.error("SchemaError: your metadata file is incorrect. "
-                     "Please, fix the next errors: {}".format(parser_errors(data)))
+        LOG.error("SchemaError: your metadata file is incorrect. "
+                  "Please, fix the next errors: {}".format(parser_errors(data)))
 
 
 if __name__ == "__main__":
@@ -49,9 +51,9 @@ if __name__ == "__main__":
         main(sys.argv[1:])
 
     except KeyboardInterrupt:
-        logger.warning("... jupyxplorer command was interrupted")
+        LOG.warning("... jupyxplorer command was interrupted")
         sys.exit(2)
     except Exception as ex:
-        logger.error('Unexpected error: %s' % ex)
+        LOG.error('Unexpected error: %s' % ex)
         sys.exit(1)
     sys.exit(0)
